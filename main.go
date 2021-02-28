e50fec1b9bb2e8dce9c8d8773baff22e5eef4034
Shawn Carter <shawn.carter@xclio.us>
2021-02-28 01:02:49 -0500

    Code now checks against 'output.txt' and will try again if the selected item exists.

--- main.go
+++ main.go
@@ -24,30 +24,59 @@ func input(path string) ([]string, error) {
 	return chars, scanner.Err()
 }
 
+func Find(slice []string, val string) (int, bool) {
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+	return -1, false
+}
+
 func main() {
+	used, err := input("output.txt")
 	chars, err := input("chars.txt")
 	if err != nil {
 		log.Fatalf("readLines: %s", err)
 	}
+	fmt.Println("Choosing a random character:")
 
-	fmt.Println("Random Choice:")
+	if len(used) == len(chars) {
+		time.Sleep(2 * time.Second)
+		fmt.Println("You played all your characters this week, nerd.")
+		os.Exit(1)
+	}
+
 	rand.Seed(time.Now().Unix())
 	message := fmt.Sprint(chars[rand.Intn(len(chars))])
-	messagebyte := []byte(message + "\n")
-	fmt.Println(message)
-
-	f, err := os.OpenFile("output.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if _, err := f.Write([]byte(messagebyte)); err != nil {
-		log.Fatal(err)
-	}
-	if err := f.Close(); err != nil {
-		log.Fatal(err)
+	_, found := Find(used, message)
+	for found {
+		fmt.Println("I chose", message, "but you played him already this week. Trying again..")
+		rand.Seed(time.Now().Unix())
+		rand.Shuffle(len(chars), func(i, j int) {
+			chars[i], chars[j] = chars[j], chars[i]
+		})
+		message = (chars[1])
+		_, found = Find(used, message)
 
 	}
-	fmt.Print("Press 'Enter' to continue")
-	bufio.NewReader(os.Stdin).ReadBytes('\n')
+	if !found {
+		fmt.Println("I choose", message)
+		messagebyte := []byte(message + "\n")
+		//fmt.Println(message)
 
+		f, err := os.OpenFile("output.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, err := f.Write([]byte(messagebyte)); err != nil {
+			log.Fatal(err)
+		}
+		if err := f.Close(); err != nil {
+			log.Fatal(err)
+
+		}
+		fmt.Println("Press 'Enter' to exit")
+		bufio.NewReader(os.Stdin).ReadBytes('\n')
+	}
 }
